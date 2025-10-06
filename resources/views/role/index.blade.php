b89c22f9bf681b7cca49a8a59b80d9d10d489ec7
RizalAnas00 <belangsumerlang@gmail.com>
2025-10-06 18:24:55 +0700

    feat: setup layout and component for view

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -1,15 +1,28 @@
-<x-app-layout>
-    <x-slot name="breadcrumb">
-        Role Management
-    </x-slot>
+@extends('role.layout')
 
-    <div class="py-12">
-        <div class="max-w-full mx-auto sm:px-5 lg:px-7">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Role Management Page
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+@section('content')
+    <x-table :headers="['ID', 'Name', 'Users', 'Actions']">
+        <?php $roles = [
+            (object) ['id' => 1, 'name' => 'Admin', 'users' => 3],
+            (object) ['id' => 2, 'name' => 'Job Seeker', 'users' => 420],
+            (object) ['id' => 3, 'name' => 'Company', 'users' => 30],
+        ]; ?>
+        @foreach ($roles as $role)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-750">
+                <td class="px-6 py-4">{{ $role->id }}</td>
+                <td class="px-6 py-4 font-bold">{{ $role->name }}</td>
+                <td class="px-6 py-4">{{ $role->users }}</td>
+                <td class="px-6 py-4">
+                    <a href="#" class="font-semibold text-green-600 dark:text-green-500 hover:underline">Detail</a> |
+                    <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a> |
+                    <form action="#" method="POST" class="inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="font-semibold text-red-600 dark:text-red-500 hover:underline"
+                            onclick="return confirm('Are you sure?')">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </x-table>
+@endsection
