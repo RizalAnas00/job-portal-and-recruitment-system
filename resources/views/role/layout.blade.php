a1393bdf0cb03dc68bb5c95174ac9980f6b6a310
RizalAnas00 <belangsumerlang@gmail.com>
2025-10-06 18:24:55 +0700

    feat: setup layout and component for view

---
+++ resources/views/role/layout.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="breadcrumb">
+        Role Management
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-full mx-auto sm:px-3 lg:px-5">
+            <div class="pb-3 text-gray-100">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+</x-app-layout>
